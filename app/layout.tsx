4c2d5e6a2126ac532c17582fbf0bfbe261ac617d
Giordan Dela Cruz <52592435+GiordanDelaCruz@users.noreply.github.com>
2025-08-11 01:13:06 -0400

    Finished up to Chapter 3.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,9 @@
+// Load global styles
+import "@/app/ui/global.css";
+
+// Load font styles
+import { inter } from "@/app/ui/fonts";
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +11,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }

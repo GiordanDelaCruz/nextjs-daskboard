c3e65d62269deb6fb516a3c9a2ac5abb8a3d78cc
Giordan Dela Cruz <52592435+GiordanDelaCruz@users.noreply.github.com>
2025-08-21 00:27:02 -0400

    Completed chapter 4.

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,18 @@
+/**
+ * Note: The purpose of the layout.tsx file is to share UI across multiple pages.
+ *       In this case, a side navigation will be shared across the dashboard,
+ *       customers, and invoices page.
+ */
+
+import SideNav from "@/app/ui/dashboard/sidenav";
+
+export default function Layout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+      <div className="w-full flex-none md:w-64">
+        <SideNav />
+      </div>
+      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+    </div>
+  );
+}
